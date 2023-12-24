f6bfa84fb50a9663d97c56a8a2a26d56d0e08b06
a-urq <amelia.r.urquhart@gmail.com>
2023-12-24 06:31:23 -0600

    Added ECAPE parcel paths! [removed main method from testing]

--- src/com/ameliaWx/ecape4j/Ecape.java
+++ src/com/ameliaWx/ecape4j/Ecape.java
@@ -8,29 +8,29 @@ import java.util.ArrayList;
  *
  */
 public class Ecape {
-	public static void main(String[] args) {
-//		double testPressure = 90000;
-//		double testTemperature = 290;
-//		double testDewpoint = 290;
-//		double testHeight = 1000;
+//	public static void main(String[] args) {
+////		double testPressure = 90000;
+////		double testTemperature = 290;
+////		double testDewpoint = 290;
+////		double testHeight = 1000;
+////
+////		double specificHumidity = Ecape.specificHumidityFromDewpoint(testDewpoint, testPressure);
+////		double specificHumidity2 = Ecape.specificHumidityFromDewpoint(testDewpoint, testPressure-10000);
+////		
+////		double resultDewpoint = Ecape.dewpointFromSpecificHumidity(specificHumidity, testPressure);
+////		
+////		System.out.println(specificHumidity);
+////		System.out.println(specificHumidity2);
+////		System.out.println(resultDewpoint);
 //
-//		double specificHumidity = Ecape.specificHumidityFromDewpoint(testDewpoint, testPressure);
-//		double specificHumidity2 = Ecape.specificHumidityFromDewpoint(testDewpoint, testPressure-10000);
-//		
-//		double resultDewpoint = Ecape.dewpointFromSpecificHumidity(specificHumidity, testPressure);
-//		
-//		System.out.println(specificHumidity);
-//		System.out.println(specificHumidity2);
-//		System.out.println(resultDewpoint);
-
-//		double moistStaticEnergy = Ecape.moistStaticEnergy(testTemperature, testDewpoint, testHeight, testPressure);
-
-//		double resultTemperature = Ecape.temperatureFromMoistStaticEnergy(moistStaticEnergy, testPressure, testHeight);
-
-//		System.out.println(resultTemperature);
-		System.out.println(updraftRadius(4539, 2993, 10, 12000));
-		System.out.println(entrainmentRate(updraftRadius(1000, 800, 10, 10000)) + " m");
-	}
+////		double moistStaticEnergy = Ecape.moistStaticEnergy(testTemperature, testDewpoint, testHeight, testPressure);
+//
+////		double resultTemperature = Ecape.temperatureFromMoistStaticEnergy(moistStaticEnergy, testPressure, testHeight);
+//
+////		System.out.println(resultTemperature);
+//		System.out.println(updraftRadius(4539, 2993, 10, 12000));
+//		System.out.println(entrainmentRate(updraftRadius(1000, 800, 10, 10000)) + " m");
+//	}
 
 	/**
 	 * Computes the value of Entrainment Cape as specified by John Peters et. al.
